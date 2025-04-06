92d72288324522da1cc6e52dec739dc1709fa420
vhsdream <punk.sand7393@fastmail.com>
2025-04-06 11:42:49 -0400

    formatting fixes

--- install.sh
+++ install.sh
@@ -7,19 +7,19 @@ set -xeuo pipefail # Make people's life easier
 # -------------------
 SCRIPT_DIR=$PWD
 
-create_install_env_file () {
-    # Check if env file exists
-    if [ ! -f $SCRIPT_DIR/.env ]; then
-        # If not, create a new one based on the template
-        if [ -f $SCRIPT_DIR/install.env ]; then
-            cp install.env .env
-            echo "New .env file created from the template, exiting"
-            exit 0
-        else
-            echo ".env.template not found, please clone the entire repo, exiting"
-            exit 1
-        fi
+create_install_env_file() {
+  # Check if env file exists
+  if [ ! -f $SCRIPT_DIR/.env ]; then
+    # If not, create a new one based on the template
+    if [ -f $SCRIPT_DIR/install.env ]; then
+      cp install.env .env
+      echo "New .env file created from the template, exiting"
+      exit 0
+    else
+      echo ".env.template not found, please clone the entire repo, exiting"
+      exit 1
     fi
+  fi
 }
 
 create_install_env_file
@@ -28,12 +28,12 @@ create_install_env_file
 # Load environment variables from env file
 # -------------------
 
-load_environment_variables () {
-    # Read the .env file into variables
-    cd $SCRIPT_DIR
-    set -a
-    . ./.env
-    set +a
+load_environment_variables() {
+  # Read the .env file into variables
+  cd $SCRIPT_DIR
+  set -a
+  . ./.env
+  set +a
 }
 
 load_environment_variables
@@ -43,21 +43,21 @@ load_environment_variables
 # -------------------
 
 set +x
-review_install_information () {
-    # Install Version
-    echo $REPO_TAG
-    # Install Location
-    echo $INSTALL_DIR
-    # Upload Location
-    echo $UPLOAD_DIR
-    # Cuda or No
-    echo $isCUDA
-    # Openvino or No
-    echo $isOPENVINO
-    # npm proxy
-    echo $PROXY_NPM
-    # poetry proxy
-    echo $PROXY_POETRY
+review_install_information() {
+  # Install Version
+  echo $REPO_TAG
+  # Install Location
+  echo $INSTALL_DIR
+  # Upload Location
+  echo $UPLOAD_DIR
+  # Cuda or No
+  echo $isCUDA
+  # Openvino or No
+  echo $isOPENVINO
+  # npm proxy
+  echo $PROXY_NPM
+  # poetry proxy
+  echo $PROXY_POETRY
 }
 
 review_install_information
@@ -66,43 +66,43 @@ review_install_information
 # Check if dependency are met
 # -------------------
 
-review_dependency () {
-    # ffmpeg
-    if ! command -v ffmpeg &> /dev/null; then
-        echo "ERROR: ffmpeg is not installed."
-    fi
+review_dependency() {
+  # ffmpeg
+  if ! command -v ffmpeg &>/dev/null; then
+    echo "ERROR: ffmpeg is not installed."
+  fi
 
-    # node.js
-    if ! command -v node &> /dev/null; then
-        echo "ERROR: Node.js is not installed."
-    fi
+  # node.js
+  if ! command -v node &>/dev/null; then
+    echo "ERROR: Node.js is not installed."
+  fi
 
-    # python3
-    if ! command -v python3 &> /dev/null; then
-        echo "ERROR: Python is not installed."
-    fi
+  # python3
+  if ! command -v python3 &>/dev/null; then
+    echo "ERROR: Python is not installed."
+  fi
 
-    # git
-    if ! command -v git &> /dev/null; then
-        echo "ERROR: Git is not installed."
-    fi
+  # git
+  if ! command -v git &>/dev/null; then
+    echo "ERROR: Git is not installed."
+  fi
 
-    # (Optional) Nvidia Driver
-    if [ $isCUDA = true ]; then
-        if ! nvidia-smi &> /dev/null; then
-            echo "ERROR: Nvidia driver is not installed, and isCUDA is set to true"
-            exit 1
-        fi
+  # (Optional) Nvidia Driver
+  if [ $isCUDA = true ]; then
+    if ! nvidia-smi &>/dev/null; then
+      echo "ERROR: Nvidia driver is not installed, and isCUDA is set to true"
+      exit 1
     fi
+  fi
 
-    # (Optional) Nvidia CuDNN
-    #
-    # Also need to have a check for certain OPENVINO dependencies - or add them to the dep scripts
+  # (Optional) Nvidia CuDNN
+  #
+  # Also need to have a check for certain OPENVINO dependencies - or add them to the dep scripts
 }
 
 review_dependency
 
-set -xeuo pipefail 
+set -xeuo pipefail
 
 # -------------------
 # Common variables
@@ -118,8 +118,8 @@ REPO_URL="https://github.com/immich-app/immich"
 # Clean previous build
 # -------------------
 
-clean_previous_build () {
-    rm -rf $INSTALL_DIR_app
+clean_previous_build() {
+  rm -rf $INSTALL_DIR_app
 }
 
 clean_previous_build
@@ -128,23 +128,23 @@ clean_previous_build
 # Common variables
 # -------------------
 
-create_folders () {
-    # No need to create source folder
-    mkdir -p $INSTALL_DIR_app
+create_folders() {
+  # No need to create source folder
+  mkdir -p $INSTALL_DIR_app
 
-    # Machine learning component
-    mkdir -p $INSTALL_DIR_ml
+  # Machine learning component
+  mkdir -p $INSTALL_DIR_ml
 
-    # Upload directory
-    if [ ! -d "$UPLOAD_DIR" ]; then
-        echo "$UPLOAD_DIR does not exists, creating one"
-        mkdir -p $UPLOAD_DIR
-    else
-        echo "$UPLOAD_DIR already exists, skip creation"
-    fi
+  # Upload directory
+  if [ ! -d "$UPLOAD_DIR" ]; then
+    echo "$UPLOAD_DIR does not exists, creating one"
+    mkdir -p $UPLOAD_DIR
+  else
+    echo "$UPLOAD_DIR already exists, skip creation"
+  fi
 
-    # GeoNames
-    mkdir -p $INSTALL_DIR_geo
+  # GeoNames
+  mkdir -p $INSTALL_DIR_geo
 }
 
 create_folders
@@ -153,20 +153,20 @@ create_folders
 # Clone the main repo
 # -------------------
 
-clone_the_repo () {
-    if [ ! -d "$INSTALL_DIR_src" ]; then
-        git clone "$REPO_URL" "$INSTALL_DIR_src" --single-branch
-    else
-        cd $INSTALL_DIR_src
-        # REMOVE all the change one made to source repo, which is sth not supposed to happen
-        git reset --hard main
-        # In case one is not on the branch
-        git checkout main
-        # Get updates
-        git pull
-        # Set the install version
-        git checkout $REPO_TAG
-    fi
+clone_the_repo() {
+  if [ ! -d "$INSTALL_DIR_src" ]; then
+    git clone "$REPO_URL" "$INSTALL_DIR_src" --single-branch
+  else
+    cd $INSTALL_DIR_src
+    # REMOVE all the change one made to source repo, which is sth not supposed to happen
+    git reset --hard main
+    # In case one is not on the branch
+    git checkout main
+    # Get updates
+    git pull
+    # Set the install version
+    git checkout $REPO_TAG
+  fi
 
 }
 
@@ -176,41 +176,41 @@ clone_the_repo
 # Install immich-web-server
 # -------------------
 
-install_immich_web_server () {
-    cd $INSTALL_DIR_src
+install_immich_web_server() {
+  cd $INSTALL_DIR_src
 
-    # Set mirror for npm
-    if [ ! -z "${PROXY_NPM}" ]; then
-        npm config set registry=$PROXY_NPM
-    fi
+  # Set mirror for npm
+  if [ ! -z "${PROXY_NPM}" ]; then
+    npm config set registry=$PROXY_NPM
+  fi
 
-    cd server
-    npm ci
-    npm run build
-    npm prune --omit=dev --omit=optional
-    cd ..
+  cd server
+  npm ci
+  npm run build
+  npm prune --omit=dev --omit=optional
+  cd ..
 
-    cd open-api/typescript-sdk
-    npm ci 
-    npm run build
-    cd ../..
+  cd open-api/typescript-sdk
+  npm ci
+  npm run build
+  cd ../..
 
-    cd web
-    npm ci 
-    npm run build
-    cd ..
+  cd web
+  npm ci
+  npm run build
+  cd ..
 
-    # Unset mirror for npm
-    if [ ! -z "${PROXY_NPM}" ]; then
-        npm config delete registry
-    fi
+  # Unset mirror for npm
+  if [ ! -z "${PROXY_NPM}" ]; then
+    npm config delete registry
+  fi
 
-    cp -a server/node_modules server/dist server/bin $INSTALL_DIR_app/
-    cp -a web/build $INSTALL_DIR_app/www
-    cp -a server/resources server/package.json server/package-lock.json $INSTALL_DIR_app/
-    cp -a server/start*.sh $INSTALL_DIR_app/
-    cp -a LICENSE $INSTALL_DIR_app/
-    cd ..
+  cp -a server/node_modules server/dist server/bin $INSTALL_DIR_app/
+  cp -a web/build $INSTALL_DIR_app/www
+  cp -a server/resources server/package.json server/package-lock.json $INSTALL_DIR_app/
+  cp -a server/start*.sh $INSTALL_DIR_app/
+  cp -a LICENSE $INSTALL_DIR_app/
+  cd ..
 }
 
 install_immich_web_server
@@ -219,10 +219,10 @@ install_immich_web_server
 # Copy build-lock
 # -------------------
 
-copy_build_lock () {
-    # So that immich would not complain
-    cd $SCRIPT_DIR
-    cp base-images/server/bin/build-lock.json $INSTALL_DIR_app/
+copy_build_lock() {
+  # So that immich would not complain
+  cd $SCRIPT_DIR
+  cp base-images/server/bin/build-lock.json $INSTALL_DIR_app/
 }
 
 copy_build_lock
@@ -231,13 +231,13 @@ copy_build_lock
 # Install Immich-machine-learning
 # -------------------
 
-install_immich_machine_learning () {
+install_immich_machine_learning() {
 
-# here is where I could install Intel OpenVINO deps if that is what is chosen at install time.
+  # here is where I could install Intel OpenVINO deps if that is what is chosen at install time.
 
-    cd $INSTALL_DIR_src/machine-learning
-    python3 -m venv $INSTALL_DIR_ml/venv
-    (
+  cd $INSTALL_DIR_src/machine-learning
+  python3 -m venv $INSTALL_DIR_ml/venv
+  (
     # Initiate subshell to setup venv
     . $INSTALL_DIR_ml/venv/bin/activate
 
@@ -267,13 +267,13 @@ install_immich_machine_learning
 
 # Honestly, I do not understand what does this part of the script does.
 
-replace_usr_src () {
-    cd $INSTALL_DIR_app
-    grep -Rl /usr/src | xargs -n1 sed -i -e "s@/usr/src@$INSTALL_DIR@g"
-    ln -sf $INSTALL_DIR_app/resources $INSTALL_DIR/
-    mkdir -p $INSTALL_DIR/cache
-    sed -i -e "s@\"/cache\"@\"$INSTALL_DIR/cache\"@g" $INSTALL_DIR_ml/app/config.py
-    grep -RlE "\"/build\"|'/build'" | xargs -n1 sed -i -e "s@\"/build\"@\"$INSTALL_DIR_app\"@g" -e "s@'/build'@'$INSTALL_DIR_app'@g"
+replace_usr_src() {
+  cd $INSTALL_DIR_app
+  grep -Rl /usr/src | xargs -n1 sed -i -e "s@/usr/src@$INSTALL_DIR@g"
+  ln -sf $INSTALL_DIR_app/resources $INSTALL_DIR/
+  mkdir -p $INSTALL_DIR/cache
+  sed -i -e "s@\"/cache\"@\"$INSTALL_DIR/cache\"@g" $INSTALL_DIR_ml/app/config.py
+  grep -RlE "\"/build\"|'/build'" | xargs -n1 sed -i -e "s@\"/build\"@\"$INSTALL_DIR_app\"@g" -e "s@'/build'@'$INSTALL_DIR_app'@g"
 }
 
 replace_usr_src
@@ -282,26 +282,26 @@ replace_usr_src
 # Install sharp and CLI
 # -------------------
 
-install_sharp_and_cli () {
-    cd $INSTALL_DIR_app
+install_sharp_and_cli() {
+  cd $INSTALL_DIR_app
 
-    # Set mirror for npm
-    if [ ! -z "${PROXY_NPM}" ]; then
-        npm config set registry=$PROXY_NPM
-    fi
+  # Set mirror for npm
+  if [ ! -z "${PROXY_NPM}" ]; then
+    npm config set registry=$PROXY_NPM
+  fi
 
-    npm install --build-from-source sharp
+  npm install --build-from-source sharp
 
-    # Remove sharp dependency so that it use system library
-    rm -rf $INSTALL_DIR_app/node_modules/@img/sharp-libvips*
-    rm -rf $INSTALL_DIR_app/node_modules/@img/sharp-linuxmusl-x64
+  # Remove sharp dependency so that it use system library
+  rm -rf $INSTALL_DIR_app/node_modules/@img/sharp-libvips*
+  rm -rf $INSTALL_DIR_app/node_modules/@img/sharp-linuxmusl-x64
 
-    npm i -g @immich/cli
+  npm i -g @immich/cli
 
-    # Unset mirror for npm
-    if [ ! -z "${PROXY_NPM}" ]; then
-        npm config delete registry
-    fi
+  # Unset mirror for npm
+  if [ ! -z "${PROXY_NPM}" ]; then
+    npm config delete registry
+  fi
 }
 
 install_sharp_and_cli
@@ -310,9 +310,9 @@ install_sharp_and_cli
 # Setup upload directory
 # -------------------
 
-setup_upload_folder () {
-    ln -s $UPLOAD_DIR $INSTALL_DIR_app/upload
-    ln -s $UPLOAD_DIR $INSTALL_DIR_ml/upload
+setup_upload_folder() {
+  ln -s $UPLOAD_DIR $INSTALL_DIR_app/upload
+  ln -s $UPLOAD_DIR $INSTALL_DIR_ml/upload
 }
 
 setup_upload_folder
@@ -321,24 +321,24 @@ setup_upload_folder
 # Download GeoNames
 # -------------------
 
-download_geonames () {
-    cd $INSTALL_DIR_geo
-    if [ ! -f "cities500.zip" ] || [ ! -f "admin1CodesASCII.txt" ] || [ ! -f "admin2Codes.txt" ] || [ ! -f "ne_10m_admin_0_countries.geojson" ]; then
-        echo "incomplete geodata, start downloading"
-        wget -o - https://download.geonames.org/export/dump/admin1CodesASCII.txt &
-        wget -o - https://download.geonames.org/export/dump/admin2Codes.txt &
-        wget -o - https://download.geonames.org/export/dump/cities500.zip &
-        wget -o - https://raw.githubusercontent.com/nvkelso/natural-earth-vector/v5.1.2/geojson/ne_10m_admin_0_countries.geojson &
-        wait
-        unzip cities500.zip
-        date --iso-8601=seconds | tr -d "\n" > geodata-date.txt
-    else
-        echo "geodata exists, skip downloading"
-    fi
+download_geonames() {
+  cd $INSTALL_DIR_geo
+  if [ ! -f "cities500.zip" ] || [ ! -f "admin1CodesASCII.txt" ] || [ ! -f "admin2Codes.txt" ] || [ ! -f "ne_10m_admin_0_countries.geojson" ]; then
+    echo "incomplete geodata, start downloading"
+    wget -o - https://download.geonames.org/export/dump/admin1CodesASCII.txt &
+    wget -o - https://download.geonames.org/export/dump/admin2Codes.txt &
+    wget -o - https://download.geonames.org/export/dump/cities500.zip &
+    wget -o - https://raw.githubusercontent.com/nvkelso/natural-earth-vector/v5.1.2/geojson/ne_10m_admin_0_countries.geojson &
+    wait
+    unzip cities500.zip
+    date --iso-8601=seconds | tr -d "\n" >geodata-date.txt
+  else
+    echo "geodata exists, skip downloading"
+  fi
 
-    cd $INSTALL_DIR
-    # Link the folder
-    ln -s $INSTALL_DIR_geo $INSTALL_DIR_app/
+  cd $INSTALL_DIR
+  # Link the folder
+  ln -s $INSTALL_DIR_geo $INSTALL_DIR_app/
 }
 
 download_geonames
@@ -347,9 +347,9 @@ download_geonames
 # Create custom start.sh script
 # -------------------
 
-create_custom_start_script () {
-    # Immich web and microservices
-    cat <<EOF > $INSTALL_DIR_app/start.sh
+create_custom_start_script() {
+  # Immich web and microservices
+  cat <<EOF >$INSTALL_DIR_app/start.sh
 #!/bin/bash
 
 export NVM_DIR="$HOME/.nvm"
@@ -363,8 +363,8 @@ cd $INSTALL_DIR_app
 exec node $INSTALL_DIR_app/dist/main "\$@"
 EOF
 
-    # Machine learning
-    cat <<EOF > $INSTALL_DIR_ml/start.sh
+  # Machine learning
+  cat <<EOF >$INSTALL_DIR_ml/start.sh
 #!/bin/bash
 
 set -a
@@ -395,19 +395,19 @@ create_custom_start_script
 # Create runtime environment file
 # -------------------
 
-create_runtime_env_file () {
-    cd $INSTALL_DIR
-    # Check if env file exists
-    if [ ! -f runtime.env ]; then
-        # If not, create a new one based on the template
-        if [ -f $SCRIPT_DIR/runtime.env ]; then
-            cp $SCRIPT_DIR/runtime.env runtime.env
-            echo "New runtime.env file created from the template, exiting"
-        else
-            echo "runtime.env not found, please clone the entire repo, exiting"
-            exit 1
-        fi
+create_runtime_env_file() {
+  cd $INSTALL_DIR
+  # Check if env file exists
+  if [ ! -f runtime.env ]; then
+    # If not, create a new one based on the template
+    if [ -f $SCRIPT_DIR/runtime.env ]; then
+      cp $SCRIPT_DIR/runtime.env runtime.env
+      echo "New runtime.env file created from the template, exiting"
+    else
+      echo "runtime.env not found, please clone the entire repo, exiting"
+      exit 1
     fi
+  fi
 }
 
 create_runtime_env_file
--- pre-install.sh
+++ pre-install.sh
@@ -14,37 +14,37 @@ REPO_URL="https://github.com/immich-app/base-images"
 BASE_IMG_REPO_DIR=$SCRIPT_DIR/base-images
 SOURCE_DIR=$SCRIPT_DIR/image-source
 LD_LIBRARY_PATH=/usr/local/lib # :$LD_LIBRARY_PATH
-LD_RUN_PATH=/usr/local/lib # :$LD_RUN_PATH
+LD_RUN_PATH=/usr/local/lib     # :$LD_RUN_PATH
 
 # -------------------
 # Git clone function
 # -------------------
 
-function git_clone () {
-    # $1 is repo URL
-    # $2 is clone target folder
-    # $3 is branch name
-    if [ ! -d "$2" ]; then
-        git clone "$1" "$2"
-    fi
-    cd $2
-    # Get updates
-    git fetch
-    # REMOVE all the change one made to source repo, which is sth not supposed to happen
-    git reset FETCH_HEAD --hard
-    # In case one is not on the branch
-    git reset --hard "$3"
+function git_clone() {
+  # $1 is repo URL
+  # $2 is clone target folder
+  # $3 is branch name
+  if [ ! -d "$2" ]; then
+    git clone "$1" "$2"
+  fi
+  cd $2
+  # Get updates
+  git fetch
+  # REMOVE all the change one made to source repo, which is sth not supposed to happen
+  git reset FETCH_HEAD --hard
+  # In case one is not on the branch
+  git reset --hard "$3"
 }
 
 # -------------------
 # Remove build folder function
 # -------------------
 
-function remove_build_folder () {
-    cd $1
-    if [ -d "build" ]; then
-        rm -r build
-    fi
+function remove_build_folder() {
+  cd $1
+  if [ -d "build" ]; then
+    rm -r build
+  fi
 }
 
 # -------------------
@@ -57,9 +57,9 @@ git_clone $REPO_URL $BASE_IMG_REPO_DIR main
 # Change build-lock permission
 # -------------------
 
-change_permission () {
-    # Change file permission so that install script could copy the content
-    chmod 666 $BASE_IMG_REPO_DIR/server/bin/build-lock.json
+change_permission() {
+  # Change file permission so that install script could copy the content
+  chmod 666 $BASE_IMG_REPO_DIR/server/bin/build-lock.json
 }
 
 change_permission
@@ -68,12 +68,12 @@ change_permission
 # Setup folders
 # -------------------
 
-setup_folders () {
-    cd $SCRIPT_DIR
+setup_folders() {
+  cd $SCRIPT_DIR
 
-    if [ ! -d "$SOURCE_DIR" ]; then
-        mkdir $SOURCE_DIR
-    fi
+  if [ ! -d "$SOURCE_DIR" ]; then
+    mkdir $SOURCE_DIR
+  fi
 }
 
 setup_folders
@@ -82,9 +82,9 @@ setup_folders
 # Build libjxl
 # -------------------
 
-change_locale () {
-    sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-    locale-gen
+change_locale() {
+  sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+  locale-gen
 }
 
 change_locale
@@ -93,34 +93,34 @@ change_locale
 # Build libjxl
 # -------------------
 
-build_libjxl () {
-    cd $SCRIPT_DIR
+build_libjxl() {
+  cd $SCRIPT_DIR
 
-    SOURCE=$SOURCE_DIR/libjxl
+  SOURCE=$SOURCE_DIR/libjxl
 
-    set -e
+  set -e
 
-    # Monitor these often
-    JPEGLI_LIBJPEG_LIBRARY_SOVERSION="62"
-    JPEGLI_LIBJPEG_LIBRARY_VERSION="62.3.0"
+  # Monitor these often
+  JPEGLI_LIBJPEG_LIBRARY_SOVERSION="62"
+  JPEGLI_LIBJPEG_LIBRARY_VERSION="62.3.0"
 
-    : "${LIBJXL_REVISION:=$(jq -cr '.sources[] | select(.name == "libjxl").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
-    set +e
+  : "${LIBJXL_REVISION:=$(jq -cr '.sources[] | select(.name == "libjxl").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
+  set +e
 
-    git_clone https://github.com/libjxl/libjxl.git $SOURCE $LIBJXL_REVISION
+  git_clone https://github.com/libjxl/libjxl.git $SOURCE $LIBJXL_REVISION
 
-    cd $SOURCE
+  cd $SOURCE
 
-    git submodule update --init --recursive --depth 1 --recommend-shallow
+  git submodule update --init --recursive --depth 1 --recommend-shallow
 
-    git apply $BASE_IMG_REPO_DIR/server/bin/jpegli-empty-dht-marker.patch
-    git apply $BASE_IMG_REPO_DIR/server/bin/jpegli-icc-warning.patch
+  git apply $BASE_IMG_REPO_DIR/server/bin/jpegli-empty-dht-marker.patch
+  git apply $BASE_IMG_REPO_DIR/server/bin/jpegli-icc-warning.patch
 
-    remove_build_folder $SOURCE
-    
-    mkdir build
-    cd build
-    cmake \
+  remove_build_folder $SOURCE
+
+  mkdir build
+  cd build
+  cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_TESTING=OFF \
     -DJPEGXL_ENABLE_DOXYGEN=OFF \
@@ -138,70 +138,70 @@ build_libjxl () {
     -DJPEGLI_LIBJPEG_LIBRARY_VERSION="${JPEGLI_LIBJPEG_LIBRARY_VERSION}" \
     -DLIBJPEG_TURBO_VERSION_NUMBER=2001005 \
     ..
-    # Move the following flag to above if one's system support AVX512
-    # -DJPEGXL_ENABLE_AVX512=ON \
-    # -DJPEGXL_ENABLE_AVX512_ZEN4=ON \
-    cmake --build . -- -j"$(nproc)"
-    cmake --install .
+  # Move the following flag to above if one's system support AVX512
+  # -DJPEGXL_ENABLE_AVX512=ON \
+  # -DJPEGXL_ENABLE_AVX512_ZEN4=ON \
+  cmake --build . -- -j"$(nproc)"
+  cmake --install .
 
-    ldconfig /usr/local/lib
+  ldconfig /usr/local/lib
 
-    # Clean up builds
-    make clean
-    remove_build_folder $SOURCE
-    rm -rf $SOURCE/third_party/
+  # Clean up builds
+  make clean
+  remove_build_folder $SOURCE
+  rm -rf $SOURCE/third_party/
 }
 
 # Experimental build, currently broken
 while getopts "e" opt; do
-    case $opt in
-        e)
-        build_libjxl
-        ;;
-        \?)
-        echo "Invalid option: -$OPTARG" >&2
-        exit 1
-        ;;
-    esac
+  case $opt in
+  e)
+    build_libjxl
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
 done
 
 # -------------------
 # Build libheif
 # -------------------
 
-build_libheif () {
-    cd $SCRIPT_DIR
+build_libheif() {
+  cd $SCRIPT_DIR
 
-    SOURCE=$SOURCE_DIR/libheif
+  SOURCE=$SOURCE_DIR/libheif
 
-    set -e
-    : "${LIBHEIF_REVISION:=$(jq -cr '.sources[] | select(.name == "libheif").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
-    set +e
+  set -e
+  : "${LIBHEIF_REVISION:=$(jq -cr '.sources[] | select(.name == "libheif").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
+  set +e
 
-    git_clone https://github.com/strukturag/libheif.git $SOURCE $LIBHEIF_REVISION
+  git_clone https://github.com/strukturag/libheif.git $SOURCE $LIBHEIF_REVISION
 
-    cd $SOURCE
+  cd $SOURCE
 
-    remove_build_folder $SOURCE
+  remove_build_folder $SOURCE
 
-    mkdir build
-    cd build
-    cmake --preset=release-noplugins \
-        -DWITH_DAV1D=ON \
-        -DENABLE_PARALLEL_TILE_DECODING=ON \
-        -DWITH_LIBSHARPYUV=ON \
-        -DWITH_LIBDE265=ON \
-        -DWITH_AOM_DECODER=OFF \
-        -DWITH_AOM_ENCODER=OFF \
-        -DWITH_X265=OFF \
-        -DWITH_EXAMPLES=OFF \
-        ..
-    make install
-    ldconfig /usr/local/lib
+  mkdir build
+  cd build
+  cmake --preset=release-noplugins \
+    -DWITH_DAV1D=ON \
+    -DENABLE_PARALLEL_TILE_DECODING=ON \
+    -DWITH_LIBSHARPYUV=ON \
+    -DWITH_LIBDE265=ON \
+    -DWITH_AOM_DECODER=OFF \
+    -DWITH_AOM_ENCODER=OFF \
+    -DWITH_X265=OFF \
+    -DWITH_EXAMPLES=OFF \
+    ..
+  make install
+  ldconfig /usr/local/lib
 
-    # Clean up builds
-    make clean
-    remove_build_folder $SOURCE
+  # Clean up builds
+  make clean
+  remove_build_folder $SOURCE
 }
 
 build_libheif
@@ -210,27 +210,27 @@ build_libheif
 # Build libraw
 # -------------------
 
-build_libraw () {
-    cd $SCRIPT_DIR
+build_libraw() {
+  cd $SCRIPT_DIR
 
-    SOURCE=$SOURCE_DIR/libraw
+  SOURCE=$SOURCE_DIR/libraw
 
-    set -e
-    : "${LIBRAW_REVISION:=$(jq -cr '.sources[] | select(.name == "libraw").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
-    set +e
+  set -e
+  : "${LIBRAW_REVISION:=$(jq -cr '.sources[] | select(.name == "libraw").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
+  set +e
 
-    git_clone https://github.com/libraw/libraw.git $SOURCE $LIBRAW_REVISION
+  git_clone https://github.com/libraw/libraw.git $SOURCE $LIBRAW_REVISION
 
-    cd $SOURCE
+  cd $SOURCE
 
-    autoreconf --install
-    ./configure
-    make -j"$(nproc)"
-    make install
-    ldconfig /usr/local/lib
+  autoreconf --install
+  ./configure
+  make -j"$(nproc)"
+  make install
+  ldconfig /usr/local/lib
 
-    # Clean up builds
-    make clean
+  # Clean up builds
+  make clean
 }
 
 build_libraw
@@ -241,26 +241,26 @@ build_libraw
 # Build image magick
 # -------------------
 
-build_image_magick () {
-    cd $SCRIPT_DIR
+build_image_magick() {
+  cd $SCRIPT_DIR
 
-    SOURCE=$SOURCE_DIR/image-magick
+  SOURCE=$SOURCE_DIR/image-magick
 
-    set -e
-    : "${IMAGEMAGICK_REVISION:=$(jq -cr '.sources[] | select(.name == "imagemagick").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
-    set +e
+  set -e
+  : "${IMAGEMAGICK_REVISION:=$(jq -cr '.sources[] | select(.name == "imagemagick").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
+  set +e
 
-    git_clone https://github.com/ImageMagick/ImageMagick.git $SOURCE $IMAGEMAGICK_REVISION
+  git_clone https://github.com/ImageMagick/ImageMagick.git $SOURCE $IMAGEMAGICK_REVISION
 
-    cd $SOURCE
+  cd $SOURCE
 
-    ./configure --with-modules
-    make -j"$(nproc)"
-    make install
-    ldconfig /usr/local/lib
+  ./configure --with-modules
+  make -j"$(nproc)"
+  make install
+  ldconfig /usr/local/lib
 
-    # Clean up builds
-    make clean
+  # Clean up builds
+  make clean
 }
 
 build_image_magick
@@ -269,29 +269,29 @@ build_image_magick
 # Build libvips
 # -------------------
 
-build_libvips () {
-    cd $SCRIPT_DIR
+build_libvips() {
+  cd $SCRIPT_DIR
 
-    SOURCE=$SOURCE_DIR/libvips
+  SOURCE=$SOURCE_DIR/libvips
 
-    set -e
-    : "${LIBVIPS_REVISION:=$(jq -cr '.sources[] | select(.name == "libvips").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
-    set +e
+  set -e
+  : "${LIBVIPS_REVISION:=$(jq -cr '.sources[] | select(.name == "libvips").revision' $BASE_IMG_REPO_DIR/server/bin/build-lock.json)}"
+  set +e
 
-    git_clone https://github.com/libvips/libvips.git $SOURCE $LIBVIPS_REVISION
+  git_clone https://github.com/libvips/libvips.git $SOURCE $LIBVIPS_REVISION
 
-    cd $SOURCE
-    
-    remove_build_folder $SOURCE
-    
-    # -Djpeg-xl=disabled is added because previous broken install will break libvips
-    meson setup build --buildtype=release --libdir=lib -Dintrospection=disabled -Dtiff=disabled -Djpeg-xl=disabled
-    cd build
-    ninja install
-    ldconfig /usr/local/lib
+  cd $SOURCE
 
-    # Clean up builds
-    remove_build_folder $SOURCE
+  remove_build_folder $SOURCE
+
+  # -Djpeg-xl=disabled is added because previous broken install will break libvips
+  meson setup build --buildtype=release --libdir=lib -Dintrospection=disabled -Dtiff=disabled -Djpeg-xl=disabled
+  cd build
+  ninja install
+  ldconfig /usr/local/lib
+
+  # Clean up builds
+  remove_build_folder $SOURCE
 }
 
 build_libvips
@@ -300,12 +300,12 @@ build_libvips
 # Remove unused packages
 # -------------------
 
-remove_unused_packages () {
-    # Dockerfile 109
-    ## Debian
-    dpkg -r --force-depends libjpeg62-turbo
-    ## Ubuntu
-    dpkg -r --force-depends libjpeg-turbo8
+remove_unused_packages() {
+  # Dockerfile 109
+  ## Debian
+  dpkg -r --force-depends libjpeg62-turbo
+  ## Ubuntu
+  dpkg -r --force-depends libjpeg-turbo8
 }
 
 # Skip this because this causes much headache down the road
